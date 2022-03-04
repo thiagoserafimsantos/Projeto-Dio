717f01b35b2544a66746dc5d8662388cd75993dc
thiagoserafimsantos <thiagoserafim0204@gmail.com>
2022-03-04 00:13:06 -0300

    primeiro commit

---
+++ src/IConta.java
@@ -0,0 +1,13 @@
+
+public interface IConta {
+
+	void sacar(double valor);
+	
+	void depositar(double valor);
+		
+	void imprimirExtrato();
+
+	void transferir(double valor, IConta contaDestino);
+
+		
+}
